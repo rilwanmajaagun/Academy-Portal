c55f9b0dd6ebcb10179c5c93742838e9f3a8fd6c
kollyd <majaagunoyinkolade@gmail.com>
2020-05-21 12:52:14 +0100

    install pg

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const db = require("./database");
 // const authRoute = require("./Routes/authRoute");
 
 let app = express();
@@ -7,9 +8,14 @@ let port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.listen(port, () => {
-    console.log(" Application Listening on Port 5000")
+    console.log(`Application Listening on Port ${port}`)
 });
 
+app.get("/", (req, res) => {
+    return res.status(200).json({
+        message: "Welcome to Academy Portal Api"
+    });
+});
 
 
 // app.use("/api/v1", authRoute);
