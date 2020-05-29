78de1d730a1a33ca8dd6f291de13d874cdf1c2c8
kollyd <majaagunoyinkolade@gmail.com>
2020-05-29 10:11:42 +0100

    update

--- route.js
+++ route.js
@@ -37,26 +37,26 @@ const {
     verifyAdminToken
 } = require("./Authorization/verification")
 
-router.post("/admin/login",loginMiddleWare,adminLogin)
-router.post("/signup",signupMiddleWare,signup);
-router.post("/login", loginMiddleWare,loginController)
-router.post("/application",verifyToken,applicationMiddleWare,applicationController)
+router.post("/admin/login",loginMiddleWare,adminLogin) //working
+router.post("/signup",signupMiddleWare,signup); //working
+router.post("/login", loginMiddleWare,loginController) //working
+router.post("/application",verifyToken,applicationMiddleWare,applicationController) //working
 router.post("/createApplication",verifyAdminToken,createapplicationMiddleWare,adminCreateApplication)
 router.post("/assessment",verifyAdminToken,Assessments)
 router.post("/userAns",verifyToken,userAnswer) 
 router.post("/academy",verifyAdminToken,createAcademyMiddleWare,createAcademy)
 router.put("/status_change", verifyAdminToken,statusChangeMiddleWare,changeApplicationController)
-router.get("/application", verifyToken, getUserApplicationController)
-router.get("/applicants/all", verifyAdminToken, getAllApplicantsResultDESCController)
-router.get("/applicants_asc/all", verifyAdminToken, getAllApplicantsResultASCController)
-router.get("/specific_batch/:batch_id", verifyAdminToken, getSpecificBatchController)
+router.get("/application", verifyToken, getUserApplicationController) //working
+router.get("/applicants/all", verifyAdminToken, getAllApplicantsResultDESCController) // remove later
+router.get("/applicants_asc/all", verifyAdminToken, getAllApplicantsResultASCController) //remove later
+router.get("/specific_batch/:batch_id", verifyAdminToken, getSpecificBatchController) //working
 router.get("/getTotal", getTotal)
 router.get("/academy", verifyAdminToken,getAllAcademyRecords)
 router.get("/academySoFar", verifyAdminToken,getAcademyNumbers)
 router.get("/score", verifyToken,userScores) // change to post wen user submit 
 router.put("/update_question", verifyAdminToken, changequestionController)
 router.get("/getQuestion", verifyToken,getQues)
-router.get("/getUpdate", getUpdatedApplication)
+router.get("/getUpdate", getUpdatedApplication) //working
 
 
 
